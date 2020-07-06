f7b152ed12ea87b7bf53aa60366eab6588d41752
Domenico Trotta <43494171+domenico97@users.noreply.github.com>
2020-07-06 14:22:46 +0200

    create model

--- src/model/MongoClass.java
+++ src/model/MongoClass.java
@@ -1,3 +1,4 @@
+package model;
 
 import static com.mongodb.client.model.Aggregates.lookup;
 import static com.mongodb.client.model.Filters.eq;
@@ -9,10 +10,13 @@ import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import com.mongodb.MongoCredential;
 import com.mongodb.MongoClientOptions;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 import org.bson.Document;
@@ -28,22 +32,28 @@ public class MongoClass {
 			        "mongodb://localhost:27017/"
 			    )
 			);
+		
 		System.out.println("Connessione stabilita");
 		MongoDatabase database = mongoClient.getDatabase("BD2");
 		MongoCollection<Document> collection = database.getCollection("Movies");
-		Document query = new Document();
-        query.append("avg_vote", new Document()
-                .append("$gt", "6")
-        );
-       
-        Consumer<Document> processBlock = new Consumer<Document>() {
-            @Override
-            public void accept(Document document) {
-                System.out.println(document);
-            }
-        };
-       
-        collection.find(query).forEach(processBlock);
+		 Document query = new Document();
+         query.append("avg_vote", new Document()
+                 .append("$gt", "6")
+         );
+        
+         Document projection = new Document();
+         projection.append("title", 1.0);
+         projection.append("country", 1.0);
+         projection.append("avg_vote", 1.0);
+         projection.append("_id", 0);
+        
+           
+         ArrayList<Document> list = collection.find(query).projection(projection).into(new ArrayList<>());
+         System.out.println(list.size());
+         for (Document x : list) {
+        	  System.out.println(x);
+         }
+        
 	}
 }
 

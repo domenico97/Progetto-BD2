af325de1b5032ee82de34e49f35d47dceb048e1b
Giulio <gvarone.10@gmail.com>
2020-07-12 17:29:21 +0200

    Created visualization of queries 3-5-7

---
+++ src/controller/IncassiGeneri.java
@@ -0,0 +1,74 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.bson.Document;
+
+import model.Movies;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * Servlet implementation class IncassiDirector
+ */
+@WebServlet("/IncassiGeneri")
+public class IncassiGeneri extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	static Movies model;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IncassiGeneri() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		String from = request.getParameter("From");
+		String to = request.getParameter("To");
+		System.out.println(from + " " + to);
+		
+			// Richiamo il metodo del model per eseguire la query
+			ArrayList<Document> result = model.getMedianIncomeByGenre(from, to);
+			System.out.println("Dimensione result" + result.size());
+			for(Document x : result)
+				System.out.println(x);
+
+			String json = null;
+			if ((result.size()) != 0) {
+				json = "[";
+				for (Document x : result) {
+					json += "{\"genere\":" + "\"" + x.get("genre") + "\"" + ",\"mediaIncasso\":" + "\"" + x.get("avgIncome") + "\""
+							+ "},";
+				}
+				json = json.substring(0, json.length() - 1);
+				json += "]";
+
+			}
+
+			response.getWriter().print(json);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

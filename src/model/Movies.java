3ce7dffe4e5e5b241bf91061b18a5499c80add4a
Domenico Trotta <43494171+domenico97@users.noreply.github.com>
2020-07-13 18:43:24 +0200

    modifiche

--- src/model/Movies.java
+++ src/model/Movies.java
@@ -37,8 +37,7 @@ public class Movies {
 	private static MongoDatabase database = mongoClient.getDatabase("BD2");
 	private static MongoCollection<Document> collection = database.getCollection("Movies");
 	private static String indexDirector = collection.createIndex(Indexes.hashed("director"));
-	
-	
+
 	// Restituisce una lista di film con i relativi incassi
 	public static ArrayList<Document> getWorldWideGrossIncome(String from, String to) {
 
@@ -282,21 +281,70 @@ public class Movies {
 
 public static ArrayList<Document> genreDistributionForYear(String from, String to) {
 	List<? extends Bson> pipeline = Arrays.asList(
-			new Document().append("$match",
-					new Document().append("$and",
-							Arrays.asList(new Document().append("year", new Document().append("$gte", from)),
-									new Document().append("year", new Document().append("$lte", to))))),
-			new Document().append("$addFields", new Document().append("qty", 1.0)),
-			new Document().append("$project",
-					new Document().append("categoria", new Document().append("$split", Arrays.asList("$genre", ", ")))
-							.append("year", 1.0).append("qty", 1.0)),
-			new Document().append("$unwind", "$categoria"),
-			new Document().append("$group",
-					new Document().append("_id", new Document().append("anno", "$year").append("genere", "$categoria"))
-							.append("total_qty", new Document().append("$sum", "$qty"))),
-			new Document().append("$project",
-					new Document().append("anno", "$_id.anno").append("genere", "$_id.genere").append("_id", 0.0)
-							.append("total_qty", 1.0)),
+            new Document()
+                    .append("$match", new Document()
+                            .append("$and", Arrays.asList(
+                                    new Document()
+                                            .append("year", new Document()
+                                                    .append("$gte", "2000")
+                                            ),
+                                    new Document()
+                                            .append("year", new Document()
+                                                    .append("$lte", "2002")
+                                            )
+                                )
+                            )
+                    ), 
+            new Document()
+                    .append("$addFields", new Document()
+                            .append("qty", 1.0)
+                    ), 
+            new Document()
+                    .append("$project", new Document()
+                            .append("categoria", new Document()
+                                    .append("$split", Arrays.asList(
+                                            "$genre",
+                                            ", "
+                                        )
+                                    )
+                            )
+                            .append("year", 1.0)
+                            .append("qty", 1.0)
+                    ), 
+            new Document()
+                    .append("$unwind", "$categoria"), 
+            new Document()
+                    .append("$group", new Document()
+                            .append("_id", new Document()
+                                    .append("anno", "$year")
+                                    .append("genere", "$categoria")
+                            )
+                            .append("total_qty", new Document()
+                                    .append("$sum", "$qty")
+                            )
+                    ), 
+            new Document()
+                    .append("$project", new Document()
+                            .append("anno", "$_id.anno")
+                            .append("genere", "$_id.genere")
+                            .append("_id", 0.0)
+                            .append("total_qty", 1.0)
+                    ), 
+            new Document()
+                    .append("$group", new Document()
+                            .append("_id", "$anno")
+                            .append("film", new Document()
+                                    .append("$push", new Document()
+                                            .append("genere", "$genere")
+                                            .append("quantit\u00E0", "$total_qty")
+                                    )
+                            )
+                    ), 
+            new Document()
+                    .append("$sort", new Document()
+                            .append("anno", 1.0)
+                    )
+    );
 			new Document().append("$sort", new Document().append("anno", 1.0).append("total_qty", -1.0)));
 	return collection.aggregate(pipeline).allowDiskUse(false).into(new ArrayList<>());
 }
